de98a8c97f3b5ed51a1a625c8cf5329202184e46
JanisHuser <huser.janis00@gmail.com>
2025-06-04 19:18:11 +0200

    add custom section "inserted epds"

--- frontend/src/views/Insert.vue
+++ frontend/src/views/Insert.vue
@@ -103,10 +103,17 @@ import { auditCreation } from '@/services/auditService'
     try {
         await insertEPD(ahvNummer.value, geburtsdatum.value, epdStamm.value, epdKontakt.value);
         await auditCreation(hashIdentity(ahvNummer.value, geburtsdatum.value), "");
+
+        const newDossier = { ahv: ahvNummer.value, dob: geburtsdatum.value };
+        const epdDossiers = JSON.parse(localStorage.getItem('epd-dossiers') || '[]');
+        epdDossiers.push(newDossier);
+        localStorage.setItem('epd-dossiers', JSON.stringify(epdDossiers));
+
         $toast.success('EPD-Daten erfolgreich eingetragen. Sie werden weitergeleitet')
         await $router.push({ path: '/search' })
     } catch (error) {
         $toast.error('Eintragen fehlgeschlagen: ' + error)
+        console.error(error)
     }
   }
   </script>
