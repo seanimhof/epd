7ae46495620316cf3037736a35a272cc083de866
Sean Imhof <seanimhof1@gmail.com>
2025-06-05 18:56:05 +0200

    remove scrollbar

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+  <div class="min-h-[calc(100vh-5rem)] flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
     <!-- Fehlermeldung -->
     <div
       v-if="!walletInstalled"
